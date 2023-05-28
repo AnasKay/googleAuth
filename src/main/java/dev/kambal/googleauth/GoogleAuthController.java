75a4d2d3ea193e1a8b716a546fe19410bca20f91
anas.kambal <kambal.anas@gmail.com>
2023-05-28 13:39:14 +0200

    added google oauth

--- src/main/java/dev/kambal/googleauth/GoogleAuthController.java
+++ src/main/java/dev/kambal/googleauth/GoogleAuthController.java
@@ -1,13 +1,22 @@
 package dev.kambal.googleauth;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/google-auth/")
 public class GoogleAuthController {
+
+    @Autowired
+    private GoogleClientSecrets clientSecrets;
+
+    @Autowired
+    private GoogleAuthorizationCodeFlow flow;
 
     @PostMapping("/api/v1/checkUser")
     public User checkUser(@RequestBody User newUser){
